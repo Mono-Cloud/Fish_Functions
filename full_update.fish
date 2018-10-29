71496b07a66194216f8a50bc2993654ea395f858
Mono-Cloud <44344247+Mono-Cloud@users.noreply.github.com>
2018-10-29 18:53:25 +0400

    Update full_update.fish

--- full_update.fish
+++ full_update.fish
@@ -1,5 +1,8 @@
+# Creates a function in fish called 'full_update' that will update a system with the package manager 'apt' installed.
+ 
+
 function full_update
 	sudo apt update and sudo apt upgrade and /n
-	sudo apt dist-upgrade
+	sudo apt dist-upgrade $argsv
 end
 
