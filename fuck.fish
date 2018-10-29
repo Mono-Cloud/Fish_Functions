ffed09f90aa9edf889538e106091d1f3567e48fa
Mono-Cloud <44344247+Mono-Cloud@users.noreply.github.com>
2018-10-29 19:08:37 +0400

    Update fuck.fish

--- fuck.fish
+++ fuck.fish
@@ -1,5 +1,11 @@
-""" An alias for fish that sudo's the previous command using the function name 'fuck' (Because that's what I say every time
-I forget to run a command as root """
+# An alias for fish that sudo's the previous 
+# command using the function name 'fuck' 
+#(Because that's what I say every time
+# I forget to run a command as root 
+
+# I should also mention how annoying
+# it is that fish doesn't have a way
+# of multiline commenting... 
 
 function fuck
 	eval sudo $history[1] $argv
